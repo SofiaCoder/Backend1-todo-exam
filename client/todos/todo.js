a5945e32773ea4359f3a6a9884633cdb159a3864
sofiaCoder <sofia.laas@gmail.com>
2023-03-28 17:19:54 +0200

    Fixed bugg - Changed order of functions

--- client/todos/todo.js
+++ client/todos/todo.js
@@ -1,46 +1,47 @@
-import addTodo from "./functions/addTodo.js";
-import addFriend from "../friends/functions/addFriend.js";
-import findFriends from "../friends/functions/findFriends.js";
-import showFriends from "../friends/functions/showFriends.js";
-import createTodoBox from "./functions/createTodoBox.js";
+import {addTodo} from "./functions/addTodo.js";
+import {addFriend} from "../friends/functions/addFriend.js";
+import {findFriends} from "../friends/functions/findFriends.js";
+import {showFriends} from "../friends/functions/showFriends.js";
+import {createTodoBox} from "./functions/createTodoBox.js";
 const nameH1 = document.querySelector('#name');
 const headline = document.querySelector('h1');
 const addBtn = document.querySelector('#addTodo');
 const saveBtn = document.querySelector('#saveTodo');
 const addFriendBtn = document.querySelector('#addFriend');
 
+nameH1.textContent = localStorage.getItem('username');
 saveBtn.style.visibility = 'hidden';
-
-//Fetch todos and username from database 
-const res = await fetch('http://localhost:5050/todo/id',
-    {credentials: "include"});
-
-    if (res.status === 401) {
-        headline.textContent = "You are outlogged"
-        setTimeout(()=> { window.location.href="/client/authentication/index.html"}, 5000)
-    };
-
-    const data = await res.json();
-
-    nameH1.textContent = data[0].username;
-
-    //Create checkboxes with buttons of fetched data
-    createTodoBox(data);
-
-    //Finds all users and displays them in a select-menu
-    findFriends();
-
-    //Adds selected friend to inlogged user, saves into database 
-    addFriendBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        addFriend()
-});
 
 //Post todos to database
 addBtn.addEventListener('click', () => addTodo());
 
+//Finds all users and displays them in a select-menu
+findFriends();
+
 //Gets inlogged users friends an displays them
 showFriends();
+
+//Adds selected friend to inlogged user, saves into database 
+addFriendBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    addFriend()
+});
+
+//Fetch todos and username from database 
+const res = await fetch('http://localhost:5050/todo/id',
+    {credentials: "include"});
+    
+if (res.status === 401) {
+    headline.textContent = "You are outlogged"
+    setTimeout(()=> { window.location.href="/client/authentication/index.html"}, 5000)
+};
+const data = await res.json();
+
+//Create checkboxes with buttons of fetched data
+createTodoBox(data);
+
+
+
 
 
 
